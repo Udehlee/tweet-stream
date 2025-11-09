975236b203c1234c0cd74c0c0100a968b9241f35
udehlee <ritzeepita@gmail.com>
2025-11-09 22:52:02 +0100

    add GenerateHashTags to generate hashTags from tweet

--- data/simulated/simulated.go
+++ data/simulated/simulated.go
@@ -3,6 +3,7 @@ package simulated
 import (
 	"fmt"
 	"math/rand"
+	"strings"
 	"sync"
 	"time"
 
@@ -41,8 +42,8 @@ func (ts *TweetService) CreateUser() (*models.User, error) {
 	return user, nil
 }
 
-// PostTweet posts a single tweet
-func (ts *TweetService) PostTweet(msg string) (*models.Tweet, error) {
+// CreateTweet creates a single tweet
+func (ts *TweetService) CreateTweet(msg string) (*models.Tweet, error) {
 	ts.mu.Lock()
 	defer ts.mu.Unlock()
 
@@ -95,6 +96,45 @@ func (ts *TweetService) DeleteTweet(tweetId string) error {
 	delete(ts.tweet, tweetId)
 	ts.logger.Info().Msgf("tweet with the id %s has been deleted successfully", tweetId)
 	return nil
+}
+
+// GetTweet returns any random tweets
+func (ts *TweetService) GetTweet() *models.Tweet {
+	ts.mu.Lock()
+	defer ts.mu.Unlock()
+
+	if len(ts.tweet) == 0 {
+		ts.logger.Info().Msg("no tweet is seen")
+		return nil
+	}
+
+	keys := make([]string, 0, len(ts.tweet))
+	for k := range ts.tweet {
+		keys = append(keys, k)
+	}
+
+	randKey := keys[rand.Intn(len(keys))]
+	return ts.tweet[randKey]
+}
+
+// GenerateHashTags generates hashtags from tweet
+func (ts *TweetService) GeneateHashTags(msg string) string {
+	var tags []string
+	words := strings.Fields(msg)
+
+	for _, w := range words {
+		if len(w) > 5 {
+			tags = append(tags, "#"+strings.Trim(w, ".,!?"))
+		}
+
+		if len(tags) >= 2 {
+			break
+		}
+	}
+
+	hashTag := strings.Join(tags, "")
+	return hashTag
+
 }
 
 // isVerifiedOrNot assigns verified or unverified  to user
